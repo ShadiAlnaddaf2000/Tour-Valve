8863130315c0d233789e753ef0ff277144f0ea9a
shadi <shadialnaddaf2000@gmail.com>
2024-08-16 10:28:49 +0300

    new new new newsasasasasac sds

--- lib/cubits/bloc_observer.dart
+++ lib/cubits/bloc_observer.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimpleBlocObserver implements BlocObserver
-{
+class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
     debugPrint("Change - >  $change ");
   }
 
   @override
-  void onClose(BlocBase bloc) {
-    // TODO: implement onClose
-  }
+  void onClose(BlocBase bloc) {}
 
   @override
   void onCreate(BlocBase bloc) {
-    // TODO: implement onCreate
     debugPrint("cubit created - >  $bloc ");
-
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
-  }
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
 
   @override
-  void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
-  }
+  void onEvent(Bloc bloc, Object? event) {}
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
-  }
-
-
-
-
+  void onTransition(Bloc bloc, Transition transition) {}
 }
